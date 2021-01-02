9d50346309866005a4830b28dad85416482bdbed
Ananya Singh <as2863@njit.edu>
2021-01-02 18:18:02 -0500

    working on BST

---
+++ binary-search-tree/binarySearchTree.js
@@ -0,0 +1,11 @@
+const treeNode = require('./treeNode');
+
+class BinarySearchTree{
+
+    constructor(){
+
+    }
+
+}
+
+module.exports = BinarySearchTree;
---
+++ binary-search-tree/treeNode.js
@@ -0,0 +1,9 @@
+class treeNode{
+
+    constructor(){
+
+    }
+
+}
+
+module.exports = treeNode;
